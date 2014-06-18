1685968d7c25bf9d32e0029430722d200220823b
Adam M. Wilson <adammichaelwilson2@gmail.com>
2014-06-18 15:11:03 -0400

    Update CapePointLANDSAT.js

--- earthengine/CapePointLANDSAT.js
+++ earthengine/CapePointLANDSAT.js
@@ -1,9 +1,23 @@
 // CapePointLANDSAT 
 
-
 //  Specify destination and run name
+var drawmap=true
 var driveFolder="ee_ZA_output";
-var run="b2599552"
+var run="r1"
+var exportfiles=true
+var verbose=true
+
+// limit overall date range  (only dates in this range will be included)
+var datestart=new Date("2010-01-01")  // default time zone is UTC
+var datestop=new Date("2011-12-31")
+
+// Identify LANDSAT collections to include in processing
+
+// create array of all products to process
+var prods =['LANDSAT/LT4_L1T_ANNUAL_GREENEST_TOA',
+            'LANDSAT/LT5_L1T_ANNUAL_GREENEST_TOA',
+            'LANDSAT/LE7_L1T_ANNUAL_GREENEST_TOA',
+            'LANDSAT/LC8_L1T_ANNUAL_GREENEST_TOA'] 
 
 
 // Get current date as string for file metadata
@@ -25,49 +39,66 @@ var maska=dem.eq(1).clip(studyArea).focal_max(500,"circle","meters").eq(1).selec
 //var mask=maska
 //addToMap(maska,{},"LandMask")
 
-function fmask(img) {
-  return(img.mask(maska));
+/////////////////////////////////////////////////////
+/// Functions & housekeeping
+
+// identify start and stop years
+var yearstart=datestart.getUTCFullYear();
+var yearstop=datestop.getUTCFullYear();
+
+// get array of years to process
+
+//function to return year+1
+var yearplus=function (x, y) { return yearstart +y ; }
+
+var years=Array.apply(0, Array(yearstop-yearstart+1)).map(yearplus);
+var nYears=years.length;
+if(verbose){print('Processing '+years)}
+
+// function to apply mask to all images in collection
+function fprocess(img) {
+  return(img.select("greenness").multiply(100).mask(maska).int8());
 }
 
-//Subset by region and time
-var startdate='2000-01-01';
-var stopdate='2010-02-28';
-var L7 = ee.ImageCollection('LANDSAT/LE7_L1T_ANNUAL_GREENEST_TOA').select("greenness")
-var L5= ee.ImageCollection('LANDSAT/LT5_L1T_ANNUAL_GREENEST_TOA').select("greenness")
-var L4 = ee.ImageCollection('LANDSAT/LT4_L1T_ANNUAL_GREENEST_TOA').select("greenness")
-
-//var ndvi= LTOA.flatten()//select("greenness")//
-
-//print(ndvi.getInfo());
-
+// filter by time
+var tfilter=ee.Filter.calendarRange(yearstart,yearstop,'year')
 
 var NDVI_PALETTE = 
     'FFFFFF,CE7E45,DF923D,F1B555,FCD163,99B718,74A901,66A000,529400,' +
     '3E8601,207401,056201,004C00,023B01,012E01,011D01,011301';
 
-var year=1982;
-for(year = 2004; year <= 2009; year+=1) {
-var N7=L7.filter(ee.Filter.calendarRange(year,year,'year')).map(fmask)
-addToMap(N7,{min:-1,max:1,palette:NDVI_PALETTE},'L7_'+year,1);
-
-var N5=L5.filter(ee.Filter.calendarRange(year,year,'year'))
-addToMap(N5,{min:-1,max:1,palette:NDVI_PALETTE},'L5_'+year,1);
+//////////////////////////////////////////////////////////
+// now get to work...
 
 
+// loop over products
+for (var i=0; i<prods.length; i ++) {
+      print(prods[i])
+      var tname=prods[i].replace("LANDSAT/", "") //extract make product name
+      
+    var ndvi = ee.ImageCollection(prods[i]).filter(tfilter).map(fprocess)
+    // if there is no images within the date range, skip it...
+    if((ndvi.getInfo().features.length)===0) continue;
+
+if(drawmap) {
+for(var year = yearstart; year <= yearstop; year+=1) {
+addToMap(ndvi,{min:-25,max:100,palette:NDVI_PALETTE},prods[i]+year,1);
 }
 
 
-if(export){
-  var filename=date+'_'+run+'_'+yearstart+yearstop+'_'+mcol+'_'+tmonth;
-  if(verbose){  print('Exporting to: '+filename)}
+if(exportfiles){
 
- exportImage(N7,tfilename,
+  var filename=date+'_'+run+'_'+tname+'_'+yearstart+yearstop;
+  if(verbose){  print('Exporting to: '+filename)}
+ print(ndvi.getInfo())
+ exportImage(ndvi,filename,
     {'maxPixels':1000000000,
     'driveFolder':driveFolder,
-    'crs': 'SR-ORG:6974', //4326
-    'scale': '926.625433055833',
-    'region': region
+    'crs': 'EPSG:4326',
+    'scale': 30,
+    'region': studyArea.geometry().coordinates().getInfo()[0]
   });
   }  
   
-  
+
+}}
