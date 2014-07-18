aed2fffff57254f31e7e3f74276ee76833174b2f
Adam M. Wilson <adammichaelwilson@gmail.com>
2014-07-18 09:54:56 -0400

    Switch to new rstudio with rmarkdown

--- examples/1_setup.R
+++ examples/1_setup.R
@@ -25,8 +25,9 @@ library(rasterVis)
 library(rgdal)
 library(reshape2)
 
+library(knitr);library(rmarkdown)
 ## load grass
-library(spgrass6)
+#library(spgrass6)
 
 ## get current working directory as an object to feed knitr
 cwd=getwd()
@@ -34,9 +35,12 @@ cwd=getwd()
 
 ########################################
 ### Define graphical parameters
+
+## 16 bit NDVI color bar
 cndvi=function(br=0.2,c1=c("darkgrey","burlywood4"),c2=c("burlywood4","darkgreen","green")){
-  at=c(seq(-1,0,len=50),seq(0.01,1,len=50))
+  at=unique(c(seq(-1,0,len=32768),seq(0,1,len=32768)))
   bg=colorRampPalette(c1)
   gr=colorRampPalette(c2)
   return(list(at=at,col=c(bg(sum(at<br)),gr(sum(at>=br)))))
 }
+ndvi.colors=cndvi()$col
