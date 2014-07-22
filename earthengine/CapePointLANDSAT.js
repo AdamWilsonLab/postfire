c1e5ba8a95f06a943c453014ed964c1e562f33ee
Adam M. Wilson <adammichaelwilson2@gmail.com>
2014-07-22 12:07:12 -0400

    Update CapePointLANDSAT.js

--- earthengine/CapePointLANDSAT.js
+++ earthengine/CapePointLANDSAT.js
@@ -1,7 +1,7 @@
 // CapePointLANDSAT 
 
 //  Specify destination and run name
-var drawmap=false;    // flag indicating whether to add the images to the map (below) or not.
+var drawmap=true;    // flag indicating whether to add the images to the map (below) or not.
 var exportfiles=true; // flag indicating whether to actually initiate the export, leve this false while testing
 
 var driveFolder="ee_ZA_output"; // name of personal google drive folder to export to (this must be unique)
@@ -9,14 +9,14 @@ var run="1b93885d00"; // any string to indicate a version.  I typically use a ha
 var verbose=true;     // print various status messages to the console (on the right)
 
 // limit overall date range  (only dates in this range will be included)
-var datestart=new Date("1978-01-01");  // default time zone is UTC
+var datestart=new Date("1986-01-01");  // default time zone is UTC
 var datestop=new Date("2014-12-31");
 
 // Identify LANDSAT collections to include in processing
 
 // create array of all products to process
-var prods =['LANDSAT/LT4_L1T_ANNUAL_GREENEST_TOA',
-            'LANDSAT/LT5_L1T_ANNUAL_GREENEST_TOA',
+var prods =[//'LANDSAT/LT4_L1T_ANNUAL_GREENEST_TOA',
+            //'LANDSAT/LT5_L1T_ANNUAL_GREENEST_TOA',
             'LANDSAT/LE7_L1T_ANNUAL_GREENEST_TOA',
             'LANDSAT/LC8_L1T_ANNUAL_GREENEST_TOA'
             ] ;
@@ -36,7 +36,7 @@ var studyArea = ee.Feature(ee.Geometry.Polygon([[18.3,-33.89], [18.3,-34.4], [18
 // Create a mask using SRTM elevation to mask out water
 var dem=ee.Image('CGIAR/SRTM90_V4');
 dem=dem.mask();//  Extract the mask which is essentially land-ocean
-var mask=dem.eq(1).clip(studyArea).focal_max(500,"circle","meters").eq(1).select([0],["MASK"]);
+var mask=dem.eq(1).focal_max(500,"circle","meters").eq(1).select([0],["MASK"]); //.clip(studyArea)
 
 /////////////////////////////////////////////////////
 /// Functions & housekeeping
@@ -55,12 +55,13 @@ var nYears=years.length;
 if(verbose){print('Processing '+years)}
 
 // function to apply mask to all images in collection
+// offset by 2 then scale by 1000 to avoid problems with nodata upon export
 function fprocess(img) {
-  return(img.select("greenness").multiply(100).int16().where(mask.eq(0),-32768))
+  return(img.select("greenness").add(2).multiply(1000).toInt16().where(mask.eq(0),0))
 }
 
-      // filter by time
-    var tfilter=ee.Filter.calendarRange(yearstart,yearstop,'year');
+// filter by time
+var tfilter=ee.Filter.calendarRange(yearstart,yearstop,'year');
 
 
 var NDVI_PALETTE = 
@@ -70,11 +71,11 @@ var NDVI_PALETTE =
 //////////////////////////////////////////////////////////
 // now get to work...
 
-//prods=prods[3];
+//var i=0
 
 // loop over products (LANDSAT versions)
 for (var i=0; i<prods.length; i ++) {
-//      print(prods[i]);
+      print(prods[i]);
       var tname=prods[i].replace("LANDSAT/", ""); //extract make product name
 
       // make an image collection, filter it to this year, and run fprocess()
@@ -85,7 +86,7 @@ for (var i=0; i<prods.length; i ++) {
       print("Found "+years.length+" Years for "+tname);
 
       // if there is no images within the date range, skip it...
-      if(years.length===0) continue;
+//      if(years.length===0) continue;
       
       // loop over years and combine to a single multiband image
       var allndvi = ndvi.filter(ee.Filter.calendarRange(parseInt(years[0]),parseInt(years[0]),'year')).
@@ -102,7 +103,7 @@ for (var i=0; i<prods.length; i ++) {
           var allndvi = allndvi.addBands(tndvi);
 
           if(drawmap) {
-              addToMap(tndvi,{min:-25,max:100,palette:NDVI_PALETTE},tname+years[y],0);
+              addToMap(tndvi,{min:1500,max:3000,palette:NDVI_PALETTE},tname+years[y],0);
           }
       }
  
@@ -116,9 +117,12 @@ for (var i=0; i<prods.length; i ++) {
            exportImage(allndvi,filename,
                 {'maxPixels':1000000000,
                 'driveFolder':driveFolder,
-                'crs': 'EPSG:32734',
-                'scale': 30,
-                'region': studyArea.geometry().coordinates().getInfo()[0]
+//               'crs': 'EPSG:32734',
+//                'scale': 30,
+//                'region': studyArea.geometry().coordinates().getInfo()[0]
+                  'crs': 'PROJ4:+proj=utm +zone=34 +south +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0',
+                  'crs_transform': '[30,0,249990,0,-30,6189390]',
+                  'dimensions': '[674,1929]'
                 });
   }  
   
